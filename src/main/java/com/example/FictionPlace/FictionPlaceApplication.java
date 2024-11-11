cc80c822e1f4b2969eb51dad107081bb7fddb733
ray-4869 <rlfgustn777@gmail.com>
2024-11-11 11:40:39 +0900

    start

---
+++ src/main/java/com/example/FictionPlace/FictionPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.FictionPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictionPlaceApplication.class, args);
+	}
+
+}
